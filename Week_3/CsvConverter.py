acd2735b9b46ac61ff769c7d29c415f64c46ee2f
thijs kok <t.j.kok@st.hanze.nl>
2022-03-17 15:26:58 +0100

    added exersize 1 and a readme

---
+++ Week_3/CsvConverter.py
@@ -0,0 +1,35 @@
+'''
+csv to json class
+'''
+import json
+
+class CsvConverter():
+    '''
+    Converts Csv to JSON and write to file
+    '''
+    def __init__(self, header):
+
+        self.header = header
+
+    def csv_to_json(self, data_lines):
+        '''
+        this method converts the CSV lines to Json lines by turning the column names into keys
+        '''
+        # get the first line of the csv file, which contains the headers
+        self.data = data_lines
+        json_file = []
+        for line in self.data:
+            if len(self.header.split(',')) != len(line.split(',')):
+                raise ValueError('the amount of headers is not equal to the datacolumns')
+
+            else:
+                json_file.append(dict(zip(self.header.split(','), [float(x) for x in line.split(',')])))
+                
+        self.json_file = json_file
+        
+    def write_to_file(self, output_name = 'dSST.json'):
+        '''
+        Writes the json data to a file
+        '''
+        with open(file=output_name, mode = 'w') as output_file:
+            json.dump(self.json_file, output_file)
---
+++ Week_3/CsvToJsonParser.py
@@ -0,0 +1,57 @@
+'''
+main file to read the csv and convert to json
+'''
+import linecache
+from CsvConverter import CsvConverter
+
+import json
+
+FILEPATH = 'dSST.csv'
+
+
+class Reader(CsvConverter):
+    '''
+    this class contains methods to read the csv file
+    '''
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.cached_lines = []
+        self.current_line = 1
+        self.total_lines = 0
+
+    def get_lines(self):
+        '''
+        open the file and read it
+        '''
+        # get the header only when the pointer is at 1 (first line)
+        if self.current_line == 1:
+            header = linecache.getline(self.file_path, self.current_line).strip()
+            self.header = header
+            self.json_writer = CsvConverter(header)
+            self.current_line += 1
+            self.total_lines += 1
+
+        # get the 5 following lines
+        count = 0
+
+        while count < 5:
+            self.cached_lines.append(linecache.getline(self.file_path, self.current_line).strip())
+            self.current_line += 1
+            self.total_lines += 1
+            count += 1
+        
+        # get next 5 lines and convert them to json as well
+        self.csv_to_json(self.cached_lines)
+
+def main():
+    file = Reader(FILEPATH)
+    file.get_lines() # get line 2 to 7
+    file.get_lines() # get line 8 to 13
+    file.get_lines() # get line 14 to 19
+    file.get_lines() # etc
+    file.get_lines() # etc
+    file.get_lines() # etc
+    file.write_to_file() # write all the cached json to an output file
+
+if __name__ == '__main__':
+    main()
